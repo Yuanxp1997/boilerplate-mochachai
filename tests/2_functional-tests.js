08d257b580dee3a55d10812792bb90fbd6b081fb
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-03-18 01:00:48 -0600

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -87,7 +87,7 @@ suite("Functional Tests", function () {
 });
 
 const Browser = require("zombie");
-Browser.site = "http://localhost:3000";
+Browser.site = "https://boilerplate-mochachai-9trp.onrender.com";
 
 suite("Functional Tests with Zombie.js", function () {
   this.timeout(5000);
